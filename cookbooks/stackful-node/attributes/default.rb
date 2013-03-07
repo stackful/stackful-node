91ac9acb008d2d08fa35e5b66766f3a0a9f09fdb
Hristo Deshev <hristo@deshev.com>
2013-03-07 18:07:48 +0200

    Stackful-node recipe that:
    * sets up the proper nodejs server account
    * installs needed npm modules
    * configures the node.js web server with Upstart monitoring
    * registers an nginx site that proxies to the node.js app.
    * starts everything.

---
+++ cookbooks/stackful-node/attributes/default.rb
@@ -0,0 +1,8 @@
+# General settings
+default['stackful-node']['app-name'] = "node-web"
+default['stackful-node']['app-home'] = "/var/www/node-web"
+default['stackful-node']['ssl-cert'] = nil
+default['stackful-node']['ssl-key'] = nil
+default['stackful-node']['app-port'] = 3000
+default['stackful-node']['user'] = 'nodejs'
+default['stackful-node']['group'] = 'nodejs'
---
+++ cookbooks/stackful-node/recipes/default.rb
@@ -0,0 +1,54 @@
+app_home = node["stackful-node"]["app-home"]
+app_name = node["stackful-node"]["app-name"]
+node_user = node["stackful-node"]["user"]
+node_group = node["stackful-node"]["group"]
+
+group node_group
+user node_user do
+  gid node_group
+end
+
+remote_directory app_home do
+  owner node_user
+  group node_group
+  files_owner node_user
+  files_group node_group
+end
+
+template "/etc/init/node-web.conf" do
+  source "upstart/node-web.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+execute "npm install" do
+  user node_user
+  group node_group
+  environment({
+    "HOME" => app_home
+  })
+  cwd app_home
+end
+
+execute "stop node-web || true"
+execute "start node-web"
+
+include_recipe 'nginx::default'
+
+["default", "000-default"].each do |unused_default|
+  nginx_site unused_default do
+    enable false
+  end
+end
+
+template "/etc/nginx/sites-available/node-web" do
+  source "nginx/node-web.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+nginx_site app_name do
+  enable true
+end
