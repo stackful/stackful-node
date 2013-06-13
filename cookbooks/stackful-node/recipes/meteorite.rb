bb95b504745044cdce15393a09836266175b71f9
Hristo Deshev <hristo@deshev.com>
2013-06-13 15:53:45 +0300

    Manually install the latest Meteor.
    Meteorite doesn't install it for us anymore.

--- cookbooks/stackful-node/recipes/meteorite.rb
+++ cookbooks/stackful-node/recipes/meteorite.rb
@@ -1,3 +1,8 @@
+execute "install meteor" do
+  command "curl https://install.meteor.com | /bin/sh"
+  not_if "which meteor"
+end
+
 execute "install meteorite" do
   command "npm install meteorite -g"
   not_if "which mrt"
